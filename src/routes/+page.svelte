fc16ebbe6d13cc6561c03ee017474d7bf133ff2f
Tobias <tobias.neuhold@outlook.de>
2026-08-18 22:00:53 +0200

    css and sizing updates, include image frame

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,37 @@
-<aside class="fixed left-10 top-34 md:flex items-start gap-10 z-100">
-  <a href="/" class="flex items-center justify-center shrink-0">
-    <img class="h-14 w-14 rounded-full object-cover" src="/icon.jpg" alt="Home" />
-  </a>
+<script>
+	import ImageFrame from '$lib/image_frame.svelte';
+</script>
 
-  <nav class="font-figne align-center text-[10vw] lg:text-[16rem] text-main-yellow pt-2">
-    <ul class="flex flex-col gap-5">
-      <li class="active">
-        <a href="/" class="hover:opacity-70 transition-opacity">
-          <span>Home</span>
-        </a>
-      </li>
-      <li>
-        <a href="/about" class="hover:opacity-70 transition-opacity">
-          <span>About</span>
-        </a>
-      </li>
-      <li>
-        <a href="/projects" class="hover:opacity-70 transition-opacity">
-          <span class="font-figne">Projects</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
+<aside class="fixed top-34 left-10 z-100 items-start gap-10 md:flex">
+	<a href="/" class="flex shrink-0 items-center justify-center">
+		<img class="h-14 w-14 rounded-full object-cover" src="/icon.jpg" alt="Home" />
+	</a>
+
+	<nav class="align-center pt-2 font-figne text-[15vw] text-main-yellow lg:text-[10vh]">
+		<ul class="flex flex-col gap-5">
+			<li class="active">
+				<a href="/" class="transition-opacity hover:opacity-70">
+					<span>Home</span>
+				</a>
+			</li>
+			<li>
+				<a href="/about" class="transition-opacity hover:opacity-70">
+					<span>About</span>
+				</a>
+			</li>
+			<li>
+				<a href="/projects" class="transition-opacity hover:opacity-70">
+					<span class="font-figne">Projects</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
 </aside>
-<div class="h-full flex items-end justify-center pb-8">
-  <div class="shadow-lg">
-    <button class="border border-white rounded-full font-bold px-8 py-2">EMAIL FORM</button>
-  </div>
+<div class="align-end flex">
+	<ImageFrame image_url="/icon.jpg" />
 </div>
-
-
+<div class="flex h-full items-end justify-center pb-8">
+	<div class="shadow-lg">
+		<button class="rounded-full border border-white px-8 py-2 font-bold">EMAIL FORM</button>
+	</div>
+</div>
