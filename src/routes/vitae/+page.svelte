771171d7ab92ab7185e158d699c781a4c7a29a18
Tobias <Tobias.neuhold@outlook.de>
2026-09-16 01:42:54 +0200

    update font, remove unused css

--- src/routes/vitae/+page.svelte
+++ src/routes/vitae/+page.svelte
@@ -5,9 +5,9 @@ import Education from '$lib/Education.svelte';
 import Experience from '$lib/Experience.svelte';
 import Skills from '$lib/Skills.svelte';
 
-import {skills} from '$lib/data/skills.ts'
-import {experience} from '$lib/data/experience.ts'
-import {education} from '$lib/data/education.ts'
+import { skills } from '$lib/data/skills';
+import { experience } from '$lib/data/experience';
+import { education } from '$lib/data/education';
 </script>
 
 <div class="relative w-full">
@@ -16,26 +16,27 @@ import {education} from '$lib/data/education.ts'
   </div>
 
   <div class="relative z-10 min-h-screen py-24 px-6 md:px-12">
-    <div class="max-w-5xl lg:w-4xl mx-auto">
+    <div class="max-w-4xl mx-auto">
       <div class="text-center mb-24">
-        <p class="font-mono text-md tracking-[0.35em] uppercase text-primary mb-5">Curriculum</p>
+        <p class="font-lora text-base tracking-[0.35em] uppercase text-primary mb-5">
+          Curriculum
+        </p>
         <h1
-          class="text-[clamp(4rem,9vw,9rem)] leading-[0.9] text-foreground mb-8"
-          style={{ fontFamily: "'Bodoni Moda', Georgia, serif", fontStyle: "italic" }}
+          class="font-bodoniamoda text-[clamp(5rem,10vw,10rem)] leading-[0.9] text-foreground mb-8 italic"
         >
           Vitae
         </h1>
-        <div class="my-8 flex justify-center">
+        <div class="my-5 flex justify-center">
           <DecoratorLine />
         </div>
-        <p class="text-primay-foreground mb-10 font-lora text-xs tracking-[0.22em] uppercase md:text-xs">
+        <p class="primary-foreground mb-10 font-lora text-xs tracking-[0.22em] uppercase md:text-xs">
           A record of my study, practice, and work — arranged in reverse order of time.
         </p>
 
         <div class="mt-4">
           <a
             href="#"
-            class="backdrop-blur-xs group font-mono-bold inline-flex items-center gap-3 border border-primary/40 px-18 py-3.5 text-sm tracking-[0.22em] text-primary uppercase transition-all duration-300 hover:bg-primary hover:text-primary-foreground"
+            class="backdrop-blur-sm group font-lora font-bold inline-flex items-center gap-3 border border-primary/40 px-18 py-3.5 text-sm tracking-[0.22em] text-primary uppercase transition-all duration-300 hover:bg-primary hover:text-primary-foreground"
           >
             Download PDF
           </a>
