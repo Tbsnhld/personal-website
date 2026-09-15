dc73cb67d165dbc0138f272c81c6ccca16107535
Tobias <Tobias.neuhold@outlook.de>
2026-09-15 23:41:52 +0200

    updated corner ornament

--- src/lib/CornerOrnamentContainer.svelte
+++ src/lib/CornerOrnamentContainer.svelte
@@ -4,9 +4,9 @@
   let { className = "" } = $props();
 </script>
 
-<figure class="{className}">
-  <div class="absolute top-0 left-0"><CornerOrnamentIcon /></div>
-  <div class="absolute top-0 right-0 scale-x-[-1]"><CornerOrnamentIcon /></div>
-  <div class="absolute bottom-0 left-0 scale-y-[-1]"><CornerOrnamentIcon /></div>
-  <div class="absolute bottom-0 right-0 scale-x-[-1] scale-y-[-1]"><CornerOrnamentIcon /></div>
-</figure>
+<div class="absolute inset-0 overflow-hidden inset-6">
+    <div class="absolute h-full top-0 left-0"><CornerOrnamentIcon /></div>
+    <div class="absolute h-full top-0 right-0 scale-x-[-1]"><CornerOrnamentIcon /></div>
+    <div class="absolute h-full bottom-0 left-0 scale-y-[-1]"><CornerOrnamentIcon /></div>
+    <div class="absolute h-full bottom-0 right-0 scale-x-[-1] scale-y-[-1]"><CornerOrnamentIcon /></div>
+</div>
